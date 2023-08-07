c60dfe4ce85f561277cda19b593f8960ac9f025b
Eric Josephat <ejosephat@gmail.com>
2023-08-07 09:51:17 -0400

    upgrade storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,22 +1,13 @@
 import type { StorybookConfig } from "@storybook/vue3-vite";
-
 const config: StorybookConfig = {
-  stories: [
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
-  ],
-  addons: [
-    "@storybook/addon-a11y",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/addon-links",
-  ],
+  stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: ["@storybook/addon-a11y", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-links", "@storybook/addon-mdx-gfm"],
   framework: {
     name: "@storybook/vue3-vite",
-    options: {},
+    options: {}
   },
   docs: {
-    autodocs: "tag",
-  },
+    autodocs: "tag"
+  }
 };
 export default config;
